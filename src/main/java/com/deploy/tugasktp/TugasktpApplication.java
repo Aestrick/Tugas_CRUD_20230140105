75727e88d867f666eba392b4c93f6315d269445c
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-03-09 13:41:00 +0700

    Initial commit

---
+++ src/main/java/com/deploy/tugasktp/TugasktpApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.tugasktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasktpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasktpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/tugasktp/TugasktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.tugasktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
