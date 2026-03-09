6c653a36281c1cea7c6c863cd76d888385976f33
Nashrul Fikri <nashrul.fikri19@gmail.com>
2026-03-09 14:04:03 +0700

    Membuat KtpDto dan KtpAddRequest untuk transfer data

---
+++ src/main/java/com/deploy/tugasktp/model/dto/KtpDto.java
@@ -0,0 +1,20 @@
+package com.deploy.tugasktp.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class KtpDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
